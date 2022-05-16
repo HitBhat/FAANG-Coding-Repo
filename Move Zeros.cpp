0b68d8d8531f483b2777c290b5f1d444de26955b
Hitesh <hbsingham17@gmail.com>
2022-05-16 10:37:34 +0530

    added first question for array test commit

---
+++ Move Zeros.cpp
@@ -0,0 +1,20 @@
+// Given an array A, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Input Format
+// First line contains integer n as size of array. next n lines contains a single integer as element of array.
+
+// Constraints
+// Note: Do this in-place without making a copy of the array and Minimize the total number of operations.
+
+// Output Format
+// Print the output Array.
+
+// Sample Input
+// 5
+// 0 
+// 1 
+// 0 
+// 3
+// 12
+// Sample Output
+// 1 3 12 0 0
